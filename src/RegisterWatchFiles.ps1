b4b1bdeda456788a328a78d8ed6d94981f1c2149
tomo3136a <tomo3136a@gmail.com>
2024-07-07 09:18:35 +0900

    modified task-schedule regiser script

--- src/RegisterWatchFiles.ps1
+++ src/RegisterWatchFiles.ps1
@@ -19,19 +19,31 @@ $WorkingDirectory = "%USERPROFILE%\documents"
 # registration task schedule
 $Trigger = @()
 $Trigger += New-ScheduledTaskTrigger `
-    -Once -at "08:00:00" `
-    -RepetitionInterval "00:01:00"
+    -Once -at (Get-Date) `
+    -RepetitionInterval (New-TimeSpan -Minutes 1)
 
 $Action = New-ScheduledTaskAction `
     -Execute $ExecutePath `
     -Argument "-m1" `
     -WorkingDirectory $WorkingDirectory
 
+$Settings = New-ScheduledTaskSettingsSet `
+    -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+
 Write-Host "Register ScheduleTask ${Name}." -ForegroundColor Yellow
 Register-ScheduledTask `
     -TaskName $Name `
     -Action $Action `
+    -Settings $Settings `
     -Trigger $Trigger | Out-Null
+
+# enable task scedule
+Write-Host "Enable ScheduleTask ${Name}." -ForegroundColor Yellow
+Get-ScheduledTask -TaskName $Name | Enable-ScheduledTask | Out-Null
+
+# start task scedule
+Write-Host "Start ScheduleTask ${Name}." -ForegroundColor Yellow
+Get-ScheduledTask -TaskName $Name | Start-ScheduledTask
 
 # display next scedule information
 Write-Host "Display ScheduleTask ${Name}." -ForegroundColor Yellow
