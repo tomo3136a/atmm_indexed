4dc0b6c18d65f5150f48b2b1d14e2e5a9dad179d
tomo3136a <tomo3136a@gmail.com>
2025-09-16 03:42:11 +0900

    modified install batch

--- lib/install.cmd
+++ lib/install.cmd
@@ -3,7 +3,9 @@ pushd %~dp0
 echo installing...
 set p=c:\opt\bin
 if not exist %p% mkdir %p%
-copy indexed.exe %p%
-%p%\indexed.exe -u
+if not exist %p%\indexed.exe (
+  copy ..\bin\indexed.exe %p%
+  %p%\indexed.exe -u
+)
 popd
 pause
